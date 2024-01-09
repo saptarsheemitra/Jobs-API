2daada03e41273356f4facbf427d4c0987efe8a6
Saptarshee Mitra <saptarshee08@gmail.com>
2024-01-10 04:21:41 +0530

    completed register func..

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,18 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+
+
 const register = async (req, res) => {
-    res.send("User registered");
+    try {
+        const user = await User.create({ ...req.body });
+        const token = user.createJWT();
+        res.status(StatusCodes.CREATED).send({ user, token });
+
+    } catch (error) {
+        res.status(StatusCodes.BAD_REQUEST).send(error);
+    }
+
+
 }
 
 const login = async (req, res) => {
